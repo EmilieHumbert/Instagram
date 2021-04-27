3ad097002cfa5d007ab77719cc225587f6c57f02
emilie <emi.m.humbert@googlemail.com>
2021-04-27 17:12:32 +0200

    Configure tailwing

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import FirebaseContext from "./context/firebase";
 import { firebase, FieldValue } from "./lib/firebase";
+import "./styles/app.css";
 
 ReactDOM.render(
   <FirebaseContext.Provider value={{ firebase, FieldValue }}>
@@ -10,5 +11,3 @@ ReactDOM.render(
   </FirebaseContext.Provider>,
   document.getElementById("root"),
 );
-
-ReactDOM.render(<App />, document.getElementById("root"));
