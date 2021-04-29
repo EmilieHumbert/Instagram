2e2c6c85547068016a38564a437c50b4276ad075
emilie <emi.m.humbert@googlemail.com>
2021-04-29 19:55:31 +0200

    Add Header component

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
 
+import Header from "../components/header";
 export default function Dashboard() {
   useEffect(() => {
     document.title = "Instagram";
   }, []);
   return (
     <div className="bg-gray-background">
+      <Header />
     </div>
   );
 }
