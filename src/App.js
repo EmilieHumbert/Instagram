8581efea0948eb878ef8551cf23da619394d125d
emilie <emi.m.humbert@googlemail.com>
2021-04-29 19:54:56 +0200

    Add use-auth-listener hook

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import * as ROUTES from "./constants/routes";
+import useAuthListener from "./hooks/use-auth-listener";
+import UserContext from "./context/user";
 
 const Login = lazy(() => import("./pages/login"));
 const SignUp = lazy(() => import("./pages/sign-up"));
@@ -9,16 +11,20 @@ const Dashboard = lazy(() => import("./pages/dashboard"));
 const NotFound = lazy(() => import("./pages/not-found"));
 
 export default function App() {
+  const { user } = useAuthListener();
+
   return (
-    <Router>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Switch>
-          <Route path={ROUTES.LOGIN} component={Login} exact />
-          <Route path={ROUTES.SIGN_UP} component={SignUp} exact />
+    <UserContext.Provider value={{ user }}>
+      <Router>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Switch>
+            <Route path={ROUTES.LOGIN} component={Login} exact />
+            <Route path={ROUTES.SIGN_UP} component={SignUp} exact />
             <Route path={ROUTES.DASHBOARD} component={Dashboard} exact />
-          <Route component={NotFound} />
-        </Switch>
-      </Suspense>
-    </Router>
+            <Route component={NotFound} />
+          </Switch>
+        </Suspense>
+      </Router>
+    </UserContext.Provider>
   );
 }
---
+++ src/hooks/use-auth-listener.js
@@ -0,0 +1,23 @@
+import { useState, useEffect, useContext } from "react";
+import FirebaseContext from "../context/firebase";
+
+export default function useAuthListener() {
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("authUser")));
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const listener = firebase.auth().onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // we have a user, therfore we can store the user in local storage
+        localStorage.setItem("authUser", JSON.stringify(authUser));
+        setUser(authUser);
+      } else {
+        // we don't have an authUser, therfore clear the local storage
+        localStorage.removeItem("authUser");
+        setUser(null);
+      }
+    });
+    return () => listener();
+  }, [firebase]);
+  return { user };
+}
