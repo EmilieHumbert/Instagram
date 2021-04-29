772b18fe253229d1d37c419a1f3e3960ff60a7da
emilie <emi.m.humbert@googlemail.com>
2021-04-29 15:08:16 +0200

    Add sign-up page and logic

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
 
 const Login = lazy(() => import("./pages/login"));
+const SignUp = lazy(() => import("./pages/sign-up"));
 
-function App() {
+export default function App() {
   return (
     <Router>
       <Suspense fallback={<p>Loading...</p>}>
         <Switch>
-          <Route path={ROUTES.LOGIN} component={Login} />
+          <Route path={ROUTES.LOGIN} component={Login} exact />
+          <Route path={ROUTES.SIGN_UP} component={SignUp} exact />
         </Switch>
       </Suspense>
     </Router>
