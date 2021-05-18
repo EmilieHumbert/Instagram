6efd506c1b52f5dfb86b627b8677710dc2634e76
emilie <emi.m.humbert@googlemail.com>
2021-05-18 18:09:01 +0200

    Fix avatar image on header

--- src/components/header.js
+++ src/components/header.js
@@ -74,7 +74,7 @@ export default function Header() {
                     <Link to={`/p/${user.displayName}`}>
                       <img
                         className="rounded-full h-8 w-8 flex"
-                        src={`/images/avatars/${user?.username}.jpg`}
+                        src={`/images/avatars/${user?.displayName.toLowerCase()}.jpg`}
                         alt={`${user.displayName} profile`}
                       />
                     </Link>
