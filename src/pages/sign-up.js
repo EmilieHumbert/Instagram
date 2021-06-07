d0a7c535e2bf8f5df20eaa23e0b76ecd56527a2b
emilie <emi.m.humbert@googlemail.com>
2021-06-07 17:59:53 +0200

    Make sign-up page responsive

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -61,13 +61,17 @@ export default function SignUp() {
 
   return (
     <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-      <div className="flex w-3/5">
+      <div className="invisible sm:visible mt-4 sm:mt-0 h-0 sm:h-auto max-w-0 sm:max-w-full flex w-3/5">
         <img src="/images/iphone-with-profile.jpg" alt="iphone with instagram app" />
       </div>
-      <div className="flex flex-col w-2/5">
+      <div className="flex flex-col mx-auto w-4/5 sm:w-2/5">
         <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
           <h1 className="flex justify-center w-full">
-            <img src="/images/logoInstaClone.png" alt="Instagram" className="mt-2 w-6/12 mb-4" />
+            <img
+              src="/images/logoInstaClone.png"
+              alt="Instagram"
+              className="mt-2 w-4/5 sm:w-6/12 mb-6 sm:mb-4"
+            />
           </h1>
           {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
           <form onSubmit={handleSignUp} method="POST">
